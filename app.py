4413aa7375389316a297a63f447005b150f6147d
ychandana7 <yedellichandana@gmail.com>
2025-07-10 10:24:50 +0530

    Committed all files

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, request, redirect, url_for
+app = Flask(__name__)
+@app.route('/', methods=['GET', 'POST'])
+def home():
+	if request.method == 'POST':
+		print("Form submitted successfully!")
+		print("Form Data:", request.form)
+		return "Registration successful!"
+	return render_template('register.html')
+if __name__ == '__main__':
+	app.run(debug=True)
